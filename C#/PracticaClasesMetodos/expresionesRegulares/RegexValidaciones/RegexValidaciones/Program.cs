c06e88fa71c4e7fd8985116ace1db8cef4141f85
Ivana Maitena Finke <156132283+IvanaFinke@users.noreply.github.com>
2025-05-09 14:58:08 -0300

    Update Program.cs

--- C#/PracticaClasesMetodos/expresionesRegulares/RegexValidaciones/RegexValidaciones/Program.cs
+++ C#/PracticaClasesMetodos/expresionesRegulares/RegexValidaciones/RegexValidaciones/Program.cs
@@ -1,4 +1,4 @@
-﻿//Programa que utiliza validaciones para una direccion de mail y una direccion web
+//Programa que utiliza validaciones para una direccion de mail y una direccion web
 
 using System.Text.RegularExpressions;
 using static System.Net.WebRequestMethods;
@@ -10,20 +10,23 @@ namespace RegexValidaciones
         static void Main(string[] args)
         {
             string mail = "ivanamaitena@gmail.com";
-            string direccionWeb = "https://www.google.com";
+            string direccionWeb = "https://www.youtube.com/watch?v=B_jD-5Dlkzo";
 
-            //Patron que busca exactamente "@" y"." seguido del @ para verificar que la direccion es valida
-            string pattern1 = @"@.*\.";
+            string pattern1 = "(@)"; //Busca si hay un @
+            string pattern2 = ".*?"; //Comprueba si hay una secuencia de caracteres (.), cero o mas veces (*) y toma los minimos posibles (?)
+            string pattern3 = "\\.?"; //Comprueba si hay un punto 
 
             //Se trata de buscar la web indentica en la base de datos
             //Agrega "?" en los casos donde no es completamente necesario que vayan esos datos para que la pagina web
             //sea reconocida
             //Se usa "?" para hacer match de zero a mas veces
-            string pattern2 = "https?://(www.)?google.com";
+            //Usa \/\/ que seria como poner //
+            //"^" indica que el enlace empieza con https: y el "$ indica que ahi termina"
+            string pattern4 = @"^https:\/\/www\.youtube\.com\/watch\?v=B_jD\-5Dlkzo$";
 
-            Regex regex1 = new Regex(pattern1, RegexOptions.IgnoreCase | RegexOptions.Singleline);
+            Regex regex1 = new Regex(pattern1+ pattern2 + pattern3, RegexOptions.IgnoreCase | RegexOptions.Singleline);
 
-            Regex regex2 = new Regex(pattern2);
+            Regex regex2 = new Regex(pattern4);
 
             //La clase Match presenta los resultados de una sola coincidencia en la comparacion, a diferencia de MatchCollection
             //la cual presenta varios conjuntos de matches
